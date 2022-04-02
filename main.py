6d8cc54055030dbcc06092a4033a61ebc17dd8d0
msochan <ms.sochan@gmail.com>
2022-04-02 12:34:55 +0200

    Add possibility to send wishes to mulitple recipients

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
 from datetime import datetime as dt
-import pandas
+import pandas as pd
 import random
 import smtplib
 
 
 # Email account credentials from where you want to send wishes
-my_email = "YOUR_EMAIL"
-password = "YOUR_PASSWORD"
-
+MY_EMAIL = "YOUR_EMAIL"
+PASSWORD = "YOUR_PASSWORD"
 
 today = dt.now()
 today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthday_person = data.loc[
+data = pd.read_csv("birthdays.csv")
+
+birthday_people = data.loc[
     (data["day"] == today_tuple[1]) & (data["month"] == today_tuple[0])
 ]
-name = birthday_person.iloc[0]["name"]
-email = birthday_person.iloc[0]["email"]
+# print(birthday_people)
 
+if birthday_people.empty:
+    print("There are no birthdays defined in birthdays.csv file for today")
+else:
+    for idx in birthday_people.index:
+        name = birthday_people['name'][idx]
+        email = birthday_people['email'][idx]
+        number = random.randint(1, 3)
+        # print(number)
+        file_name = f"letter_templates/letter_{number}.txt"
+        # print(file_name)
+        with open(file_name) as file:
+            text_data = file.read()
+            final_data = text_data.replace("[NAME]", name)
+            # print(final_data)
 
-if not birthday_person.empty:
-    number = random.randint(1, 3)
-    # print(number)
-    file_name = f"letter_templates/letter_{number}.txt"
-    # print(file_name)
-    with open(file_name) as file:
-        text_data = file.read()
-        final_data = text_data.replace("[NAME]", name)
-        # print(final_data)
-
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=password)
-            connection.sendmail(
-                from_addr=my_email,
-                to_addrs=email,
-                msg=f"Subject:Happy Birthday!\n\n{final_data}",
-            )
+            try:
+                with smtplib.SMTP("smtp.gmail.com") as connection:
+                    connection.starttls()
+                    connection.login(user=MY_EMAIL, password=PASSWORD)
+                    connection.sendmail(
+                        from_addr=MY_EMAIL,
+                        to_addrs=email,
+                        msg=f"Subject:Happy Birthday!\n\n{final_data}",
+                    )
+            except smtplib.SMTPAuthenticationError as err:
+                print(err)
